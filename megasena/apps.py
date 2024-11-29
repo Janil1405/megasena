fcd9e5f1ee05c75797a2b2ce70e351a9a7f4f6b8
remidio2015 <remidio2030@gmail.com>
2024-11-28 21:16:05 -0300

    Revert "projeto megasena"
    This reverts commit 5c64437c1c2c6b95a8667bdec7a21abb96953f8a.

--- megasena/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MegasenaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'megasena'
--- megasena/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Sorteio(models.Model):
-    data = models.DateField()
-    numeros = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f"Sorteio {self.data} - Números: {self.numeros}"
-    
